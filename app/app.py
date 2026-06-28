cb91f01a7ba407f662b774a5984300efa1bd3ac6
Sebastian Gonzalez <sa.gonzalezt@duocuc.cl>
2026-06-28 17:16:14 -0400

    listoo

--- app/app.py
+++ app/app.py
@@ -3,26 +3,16 @@ from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
 
 app = Flask(__name__)
 
-# =========================
-# MÉTRICAS PROMETHEUS
-# =========================
-
-# Contador de peticiones
 REQUEST_COUNT = Counter(
     "peticiones_totales",
     "Cantidad total de peticiones al microservicio"
 )
 
-# Contador de errores (NUEVO - NECESARIO PARA EVALUACIÓN 3)
 ERROR_COUNT = Counter(
     "errores_totales",
     "Cantidad total de errores del microservicio"
 )
 
-
-# =========================
-# ENDPOINT PRINCIPAL
-# =========================
 
 @app.route("/")
 def home():
@@ -33,19 +23,11 @@ def home():
     })
 
 
-# =========================
-# ENDPOINT DE ERROR (NUEVO)
-# =========================
-
 @app.route("/error")
 def error():
     ERROR_COUNT.inc()
     return "Error simulado", 500
 
-
-# =========================
-# MÉTRICAS PROMETHEUS
-# =========================
 
 @app.route("/metrics")
 def metrics():
@@ -53,10 +35,6 @@ def metrics():
         "Content-Type": CONTENT_TYPE_LATEST
     }
 
-
-# =========================
-# MAIN
-# =========================
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
